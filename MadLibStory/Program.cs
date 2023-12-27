59bca919630535253e50c8edcc59df4c537bc5cb
Dillon Meacham <meachamdillon52@gmail.com>
2023-12-27 17:31:07 -0600

    edited comments

--- MadLibStory/Program.cs
+++ MadLibStory/Program.cs
@@ -12,14 +12,14 @@ namespace MadLibs
       */
 
 
-      // Let the user know that the program is starting:
+     
       Console.WriteLine("Starting Mad Lib...");
 
-      // Give the Mad Lib a title:
+      // MadLib Title
       string title = "The Protest";
 
       Console.WriteLine(title);
-      // Define user input and variables:
+      // user input and variables:
       Console.Write("Enter a name: ");
       string name = Console.ReadLine();
 
@@ -66,12 +66,13 @@ namespace MadLibs
 
 
 
-      // The template for the story:
+      // story
 
       string story = $"This morning {name} woke up feeling {adj1}. 'It is going to be a {adj2} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {noun1}, which made all the {fruit}s very {adj3}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.";
 
 
       // Print the story:
+
       Console.WriteLine(story);
     }
   }
